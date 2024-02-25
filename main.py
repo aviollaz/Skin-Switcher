6d1387e884a71156dda87e6f621aafbd7dbdf7ec
AV <66049742+aviollaz@users.noreply.github.com>
2024-02-25 16:13:45 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import sys
 import os
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFileDialog
-from shutil import copytree
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFileDialog, QInputDialog
+from shutil import copytree, move
 import keyboard
 
 last_id = -1
-current_skin = -1
+current_skin_id = -1
 osu_path = ""
 multi_skin_path = ""
+skin_id_of_keybind = {}
 
 class SkinSwitcher(QWidget):
     def __init__(self):
@@ -56,7 +57,46 @@ class SkinSwitcher(QWidget):
 
         self.copy_skin_files(original_skin_directory, skin_id)
 
+        keybinds = ['ctrl+5', 'ctrl+6', 'ctrl+7', 'ctrl+8', 'ctrl+9', 'ctrl+0']
+        keybind, ok = QInputDialog.getItem(self, 'choose keybind', 'select a keybind for this skin', keybinds, 0, True)
+        if ok:
+            self.handle_key_map(keybind, skin_id)
+            
     
+    # saves the skins id for that specific keybind
+    def handle_key_map(self, keybind, skin_id):
+        skin_id_of_keybind[keybind] = skin_id
+
+
+    def detect_input(self, event):
+        if event.event_type == keyboard.KEY_DOWN:
+            if keyboard.is_pressed('ctrl') and event.name in ['5', '6', '7', '8', '9', '0']:
+                self.switch_skin(current_skin_id, skin_id_of_keybind[f'ctrl+{event.name}'])
+                print(f'pressed ctrl+{event.name}, switching skin')
+
+
+    def switch_skin(self, current_skin_id, new_skin_id):
+        # Return current skin files into its folder
+        files = os.listdir(multi_skin_path)
+
+        for file in files:
+            if not file.startswith("skin_"):
+                source_path = os.path.join(multi_skin_path, file)
+                destination_path = os.path.join(f'{multi_skin_path}/skin_{current_skin_id}', file)
+                move(source_path, destination_path)
+        
+        # Move the new skin files into the multi skin folder
+        new_files = os.listdir(f'{multi_skin_path}/skin_{new_skin_id}')
+        
+        for file in new_files:
+            source_path = os.path.join(f'{multi_skin_path}/skin_{new_skin_id}', file)
+            destination_path = multi_skin_path
+            move(source_path, destination_path)
+
+        # Update the skin id     
+        current_skin_id = new_skin_id
+
+
     def copy_skin_files(self, directory, skin_id):
         source_path = directory
         destination_path = f'{multi_skin_path}/skin_{skin_id}' 
@@ -72,10 +112,13 @@ class SkinSwitcher(QWidget):
         # Extract numbers from folder names and find the highest one
         last_id = max((int(folder.split('_')[-1]) for folder in folders if folder.startswith('skin_')), default=-1) + 1
 
-        return last_id
+        return last_id    
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     skin_switcher_app = SkinSwitcher()
     skin_switcher_app.show()
+    keyboard.hook(skin_switcher_app.detect_input)
     sys.exit(app.exec())
+    
